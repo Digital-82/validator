b6deab5afff9a9f8c888562cf01337e006736068
Artyom Bronnikov <55315424+Digital-82@users.noreply.github.com>
2023-02-21 00:33:32 +0300

    допиленое

--- index.php
+++ index.php
@@ -5,8 +5,7 @@ include('validation_cards.php');
 
 <html>
 <head>
-    <link rel="stylesheet" href="style.css">
-    <title>Card Validation</title>
+    <title>валидатор</title>
 </head>
 <body>
 <form method="post">
@@ -17,16 +16,18 @@ include('validation_cards.php');
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['cardNumber'])) {
     $cardValidation = new CardValidation();
     $result = $cardValidation->validate($_POST['cardNumber']);
-
-    //   echo 'The card is ' . $result[0] . ' and it is ' . $result[1];
 }
 ?>
-<p class="result">The card is <?php echo $result[0]; ?> and it is <span><?php echo $result[1]; ?></span></p>
+<p class="result"><?php echo $result[0]; ?>, эмитент: <span><?php echo $result[1]; ?></span></p>
 <ul>
     <li>4750657776370372 - валидная, VISA</li>
-    <li>4023 9019 5678 9014 - валидная, VISA</li>
+    <li>4750 6577 7637 0372 - валидная, VISA</li>
+    <li>4023 9019 5678 9014 - валидная, VISA ????</li>
     <li>5569191777864116 - валидная, MasterCard</li>
     <li>725163728819929 - невалидная</li>
+    <li>2202205308519153 - мир</li>
+    <li></li>
+
 </ul>
 </body>
 </html>
--- validation_cards.php
+++ validation_cards.php
@@ -1,14 +1,8 @@
 <?php
 
 class CardValidation {
-
     public function validate($cardNumber) {
-        function clear($val){
-            $val = trim($val);
-            return $val;
-        }
-        $cardNumber = clear($cardNumber);
-        // Проверка контрольной суммы по стандарту ISO/IEC 7812
+        $cardNumber = str_replace(' ', '', $cardNumber);
         $sum = 0;
         $length = strlen($cardNumber);
         $str = str_split($cardNumber);
@@ -25,17 +19,21 @@ class CardValidation {
         }
 
         if ($sum % 10 == 0) {
-            $isValid = 'valid';
+            $isValid = 'валидная';
         } else {
-            $isValid = 'not valid';
+            $isValid = 'невалидная';
         }
         // Проверка эмитента карты
         if (preg_match('/^(41|42|43|44|45|46|47|48|49|14)/', $cardNumber)) {
             $emitent = 'VISA';
         } elseif (preg_match('/^(51|52|53|54|55|62|67)/', $cardNumber)) {
             $emitent = 'MasterCard';
+        } elseif (preg_match('/^3[47]/', $cardNumber)) {
+            $emitent = 'American Express';
+        } elseif (preg_match('/^2/', $cardNumber)) {
+            $emitent = 'Мир';
         } else {
-            $emitent = 'Unknown';
+            $emitent = 'название эмитента не определено';
         }
         return [$isValid, $emitent];
     }
